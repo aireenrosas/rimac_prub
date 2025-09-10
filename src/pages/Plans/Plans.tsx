cea31ad043aa406b6202c94c32cf4bcf32a4cbbe
Aireen Rosas <aireen.rosas@ucsp.edu.pe>
2025-09-10 14:49:48 -0500

    adding more responsive styles

--- src/pages/Plans/Plans.tsx
+++ src/pages/Plans/Plans.tsx
@@ -50,6 +50,7 @@ export default function Plans() {
      ...user,
       plan,
     });
+    setActiveStep(2);
   }
 
   return (
@@ -181,7 +182,7 @@ export default function Plans() {
                   <div className="d-none d-md-block">
                     <div className="row row-cols-1 row-cols-md-3 g-2 mt-4 justify-content-center">
                         {planes.map((plan:any) => 
-                          <div className="col d-flex" key={plan.id}>
+                          <div className="col d-flex mb-4" key={plan.id}>
                             <div className="card list-plans mb-2 rounded-3 shadow-sm h-100 w-100 d-flex flex-column">
                               <div className="card-header py-3 d-flex align-items-center">
                                 <div>
@@ -252,7 +253,7 @@ export default function Plans() {
                       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-people" viewBox="0 0 16 16">
                         <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
                       </svg>
-                      {user?.name} {user?.lastName}
+                      &nbsp;{user?.name} {user?.lastName}
                     </div>
                     <div className="card-body">
                       <ul className="list-unstyled mt-3 mb-4">
